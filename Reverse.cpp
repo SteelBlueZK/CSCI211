9378a125ce58f3726c5a7db9b10539407518e6d8
SteelBlueZK <sbluefaf@gmail.com>
2022-02-10 19:42:45 -0500

    Create Reverse.cpp

---
+++ Reverse.cpp
@@ -0,0 +1,14 @@
+/*4. Write a program that will continuously ask the user for positive 
+integers (until the user enters a negative integer at which the 
+program will be terminated). Every time the user inputs a positive 
+integer the program will print out this integer in reverse. Your 
+program should have a function that accepts and integer and returns 
+an integer in reverse. To convert the integer to its reverse, your 
+program will call this function.  
+  */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+}
