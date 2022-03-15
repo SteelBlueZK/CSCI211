3222006c543f2a90fff1888b04fb93c5f05a869d
Linuxterm <grma5636@venus.cs.qc.cuny.edu>
2022-03-15 01:48:44 -0400

    Recent Advancements

--- Homework3/ContactDriver.cpp
+++ Homework3/ContactDriver.cpp
@@ -1,35 +1,44 @@
 #include <string>
 #include <iostream>
 #include "Contact.h"
+#include "Address.h"
 
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
 
+static const int LIST_SIZE = 10;//POSITIVE NUMBER
+
+static string command;
+string SolicitInst(const string& text);
+
 int main(){
-	string loop;
-	Contact 
-		zero("Sirname", "Givenname", "123 Wallaby Way Sidney Australia", "1234567890", "givenname.sirname23@self.info"),
-		one,
-		two;
-	one.setLast("Greene");
-	one.setFirst("Matthew");
-	one.setAddr("1 Great Street");
-	one.setPhone("MyPhoneNumberLOL");
-	one.setEmail("What could my email be? I don't remember.");
-	cout << zero.getID() << zero.getFirst() << zero.getLast() 
-		<< zero.getAddr() << zero.getPhone() 
-		<< zero.getEmail() <<endl;
-	one.Output();
-	two.Output();
+	Contact contacts[LIST_SIZE];
+	int numContacts = 0;
 	do {
-		Contact free;	
-		free.Input();
-		free.Output();
-		
-		cout << "Continue? Y/N";
-		cin >> loop;
-	} while (loop == "Y" || loop == "y");
+		cout << "ENTER CONTACTS FOR "<< contacts[numContacts].getID() <<"\n";
+		contacts[numContacts].input();
+		command = SolicitInst("Continue entering contacts? Y/N");
+		numContacts++;
+	} while (numContacts < LIST_SIZE && (command == "Y" || command == "y") ); 
+	//menu
+	cout 
+		<< "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl
+		<< "          Main Menu" << endl
+		<< " Select an option to continue:" << endl
+		<< "   a. Search\n"
+		<< "   b. Display All\n"
+		<< "   e. Exit\n"
+		<< "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	command = SolicitInst("enter option character");
+
 	return 0;
+}
+
+string SolicitInst(const string& text) {
+	string result;
+	cout << text << ": ";
+	cin >> result;
+	return result;
 }
