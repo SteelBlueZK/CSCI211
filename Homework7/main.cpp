6ee5555daa6d2c8861d5c8e562f79c6b96759a97
Linuxterm <grma5636@venus.cs.qc.cuny.edu>
2022-05-05 18:12:13 -0400

    SafeMatrix class fully implemented
    test program that uses a safematrix int and safematrix double provided

--- Homework7/main.cpp
+++ Homework7/main.cpp
@@ -1,9 +1,46 @@
 #include "safematrix.h"
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <cmath>
 using namespace std;
 
 int main(){
-	SafeMatrix<int> x(4,6);
+	cout << setprecision(4);
+	int ROWS(10), COLS(15);
+	SafeMatrix<int> x(ROWS,COLS);
+	cout << "number of rows is " << ROWS
+		<< " and number of colunms is "<< COLS << endl;
+	cout << "matrix.length is " << x.length() << '\n'
+		<< "matrix[0].length is " << x[0].length() << "\n";
+	for (int i = 0; i < ROWS; i++) {
+		cout << "R#" << i << "\t|";
+		for (int j = 0; j < COLS; j++) {
+			if (i%2 == 0)
+				x[i][j] = j*i;
+			else
+				x[i][j] = i + j;
+			cout << x[i][j]<< "\t|";
+		}
+		cout << endl;
+	}
+	cout << "the value of row 5(odd), colunm 10 should be row + colunm: " << x[5][10] << "\n";
+	cout << "the value of row 6(even), colunm 10 should be row * colunm: " << x[6][10] << "\n";
+	cout << "example of double:\n";
+	SafeMatrix<double> y(ROWS,COLS);
+	cout << "number of rows is " << ROWS
+		<< " and number of colunms is "<< COLS << endl;
+	cout << "matrix.length is " << y.length() << '\n'
+		<< "matrix[0].length is " << y[0].length() << "\n";
+	for (int i = 0; i < ROWS; i++) {
+		cout << "R#" << i << "\t|";
+		for (int j = 0; j < COLS; j++) {
+			y[i][j] = sqrt(i) * 2 * j;
+			cout << y[i][j]<< "\t|";
+		}
+		cout << endl;
+	}
+	cout << "the value of row 6, colunm 10 should be (sqrt(row) *2colunm): " << y[6][10] << "\n";
+
 	return 0;
 }
--- Homework7/safematrix.h
+++ Homework7/safematrix.h
@@ -5,8 +5,7 @@
 template<class T>
 class SafeMatrix {
 	private:
-		SafeArray<T> **matrix;
-		int rows;
+		SafeArray<SafeArray<T>> matrix;
 	public:
 		SafeMatrix();
 		SafeMatrix(int rows, int colunms);
@@ -16,7 +15,47 @@ class SafeMatrix {
 
 		~SafeMatrix();
 		SafeMatrix(const SafeMatrix<T>& other);
-		SafeArray<T>& operator =(const SafeArray<T>& other);
+		SafeMatrix<T>& operator =(const SafeMatrix<T>& other);
 };
+
+template <class T>
+SafeMatrix<T>::SafeMatrix() : matrix(){
+}
+
+template <class T>
+SafeMatrix<T>::SafeMatrix(int rows, int colunms) : matrix( rows) {
+	for (int i = 0; i < rows; i++) {
+		matrix[i] = SafeArray<T>(colunms);
+	}
+}
+
+template <class T>
+int SafeMatrix<T>::length() const {
+	return matrix.length();
+}
+
+template <class T>
+SafeArray<T>& SafeMatrix<T>::operator [](int index) {
+	return matrix[index];
+}
+
+template <class T>
+const SafeArray<T>& SafeMatrix<T>::operator [](int index) const {
+	return matrix[index];
+}
+
+template <class T>
+SafeMatrix<T>::~SafeMatrix() {
+}
+
+template <class T>
+SafeMatrix<T>::SafeMatrix(const SafeMatrix<T>& other) : matrix(other.matrix){
+}
+
+template <class T>
+SafeMatrix<T>& SafeMatrix<T>::operator =(const SafeMatrix<T>& other) {
+	matrix = other.matrix;
+}
+
 
 #endif
